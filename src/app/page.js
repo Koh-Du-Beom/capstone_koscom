51e893484bead946225a73150e176a7ac142480a
Koh-Du-Beom <140967208+Koh-Du-Beom@users.noreply.github.com>
2024-10-01 21:00:00 +0900

    홈페이지 얼추 구현

--- src/app/page.js
+++ src/app/page.js
@@ -2,30 +2,9 @@
 import InterestedItemsBox from '@/components/interested-items/interested-items-box';
 import { useState } from 'react';
 import classes from './page.module.css';
+import InterestedNewsBox from '@/components/interested-news/interested-news-box';
 
 export default function Home() {
-  const [searchTerm, setSearchTerm] = useState('');
-  const [news, setNews] = useState([]);
-
-  const handleSearch = async () => {
-    try {
-      const trimmedSearchTerm = searchTerm.trim();
-      if (!trimmedSearchTerm) {
-        console.error("검색어를 입력해주세요.");
-        return;
-      }
-
-      const response = await fetch(`/api/news?search=${encodeURIComponent(trimmedSearchTerm)}`);
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-
-      const data = await response.json();
-      setNews(data.headlines || []);
-    } catch (error) {
-      console.error("Error fetching news:", error);
-    }
-  };
 
   return (
     <main className={classes.container}>
@@ -36,20 +15,8 @@ export default function Home() {
       <div className={classes.divider}></div>
 
       <div className={classes.rightSection}>
-        <h3>나만의 뉴스</h3>
-        <textarea
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          placeholder="검색어를 입력하세요."
-        />
-        <button onClick={handleSearch}>뉴스 검색</button>
-
-        <h3>검색 결과:</h3>
-        <ul>
-          {news.map((item, index) => (
-            <li key={index}>{item.title}</li>
-          ))}
-        </ul>
+        
+				<InterestedNewsBox />
       </div>
     </main>
   );
--- src/components/interested-news/interested-news.js
+++ src/components/interested-news/interested-news.js
@@ -1,46 +1,16 @@
 import classes from './interested-news.module.css';
 
-export default function InterestedNews() {
-	return (
-		<div className={classes.container}>
-			<div className={classes.logo}>
+export default function InterestedNews({ news }) {
+  return (
+    <div className={classes.container}>
+      <div className={classes.logo}>
+        <img src={news.logo} alt="news logo" />
+      </div>
 
-			</div>
-		
-			<div className={classes.wrapper}>
-				<h1 className={classes.title}></h1>
-				<h3 className={classes.infos}>
-					
-				</h3>
-			</div>
-
-		</div>
-	)
-};
-
-<div className={classes.container} key={index}>
-          <div className={classes.logo}>
-            <Image 
-							src={LogoTmp}
-							alt="tmpLogo"
-							width={100}
-							height={100}
-						/>
-          </div>
-          <div className={classes.wrapper}>
-            <div className={classes.title_box}>
-              <h1 className={classes.big_title}>{item.name}</h1>
-              <h3 className={classes.small_title}>{item.code}</h3>
-            </div>
-
-            <div className={classes.price_info}>
-              <div className={classes.current_price}>{item.currentPrice}</div>
-              <div className={classes.price_change_box}>
-                <h4 className={item.priceChange > 0 ? 'text-danger' : 'text-primary'}>
-                  {item.priceChange} ({item.percentagechange}%)
-                </h4>
-                <span className={`${classes.arrow} ${item.priceChange < 0 ? classes.arrowUp : classes.arrowDown}`}></span>
-              </div>
-            </div>
-          </div>
-        </div>
+      <div className={classes.wrapper}>
+        <h1 className={classes.title}>{news.title}</h1>
+        <h3 className={classes.infos}>{news.date} - {news.source}</h3>
+      </div>
+    </div>
+  );
+}
