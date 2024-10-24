5da17a6e7fbe3f6f6d1f9ec8a51086b7339b4e95
Koh-Du-Beom <140967208+Koh-Du-Beom@users.noreply.github.com>
2024-10-25 00:53:28 +0900

    csv parser making

--- src/app/financeData/page.js
+++ src/app/financeData/page.js
@@ -1,5 +1,5 @@
 'use client';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import FinancialDropdown from '@/components/graphs/financial-data/dropdown/financial-dropdown';
 import classes from './page.module.css';
 import FinancialGraph from '@/components/graphs/financial-graph';
@@ -7,11 +7,13 @@ import InterestedItemsBox from '@/components/interested-items/interested-items-b
 import dropdownData from '@/components/graphs/financial-data/dropdown/financial-dropdown-data';
 import SelectedStock from '@/components/graphs/selected-stock';
 import FinancialPrompt from '@/components/graphs/financial-data/prompt/financial-prompt';
+import graph_Mock_data from '@/components/graphs/graphData'; // 임시 JSON 데이터 import
 
 export default function FinancialDataShowPage() {
   const [selectedOption, setSelectedOption] = useState('dropdown');
   const [checkedItems, setCheckedItems] = useState({});
   const [selectedStocks, setSelectedStocks] = useState([]); 
+  const [graphData, setGraphData] = useState([]); // JSON 데이터를 관리하는 새로운 상태
 
   // 체크박스 선택 시 상태 업데이트 핸들러
   const handleCheckboxChange = (event) => {
@@ -30,6 +32,11 @@ export default function FinancialDataShowPage() {
   const handleSelectStock = (stocks) => {
     setSelectedStocks(stocks); 
   };
+
+  // 임시로 graphData 상태를 graph_Mock_data로 설정
+  useEffect(() => {
+    setGraphData(graph_Mock_data);
+  }, []); // 추후에, 이벤트가 발생하면 데이터를 json으로 변경하는 로직 만들기
 
   return (
     <div className={classes.container}>
@@ -64,9 +71,7 @@ export default function FinancialDataShowPage() {
               />
             ))
           ) : (
-           
-						<FinancialPrompt />
-            
+            <FinancialPrompt />
           )}
         </div>
       </div>
@@ -74,7 +79,7 @@ export default function FinancialDataShowPage() {
       <div className={classes.rightSection}>
         <SelectedStock onSelectStock={handleSelectStock} />
         <div className={classes.graphSection}>
-          <FinancialGraph selectedStockNames={selectedStocks} selectedIndicators={checkedItems} />
+          <FinancialGraph graphData={graphData} />
         </div>
       </div>
     </div>
