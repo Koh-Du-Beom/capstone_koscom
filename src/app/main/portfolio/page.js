9a27e3f9b976b63a17130d8e560ea47c35666c00
Koh-Du-Beom <140967208+Koh-Du-Beom@users.noreply.github.com>
2024-12-03 16:09:46 +0900

    iframe으로 그래프 띄우기 시도

--- src/app/main/portfolio/page.js
+++ src/app/main/portfolio/page.js
@@ -1,13 +1,32 @@
-// PortfolioPage.js
 'use client'
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 
 export default function PortfolioPage() {
- 
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://s3.tradingview.com/external-embedding/embed-widget-symbol-overview.js';
+    script.async = true;
+    script.innerHTML = JSON.stringify({
+      "symbols": [
+        ["Apple", "NASDAQ:AAPL"]
+      ],
+      "chartOnly": false,
+      "width": 600,
+      "height": 400,
+      "locale": "kr",
+      "colorTheme": "light",
+      "autosize": false,
+      "showVolume": true,
+      "showMA": true,
+    });
+    
+    document.getElementById('tradingview-widget').appendChild(script);
+  }, []);
+
   return (
     <div>
       <h1>Portfolio 페이지</h1>
-  
+      <div id="tradingview-widget"></div>
     </div>
   );
 }
